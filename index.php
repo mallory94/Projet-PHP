7f7b961ff68a05012d5f457656081f088d02cb38
Anthony RJ <anthonyjoaquim40@gmail.com>
2019-10-02 18:58:37 +0200

    inseriton d'un formulaire de connexion dans index.php

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
-<?php
-	$test = "test";
-	echo $test;
-?>
+
+<form method="get" action="connect.php">
+	<fieldset>
+		<legend>Login : </legend>
+			<input type="text" name="identifiant"/>
+	</fieldset>
+	<fieldset>
+		<legend>Mot de passe : </legend>
+			<input type="password" name="motdepasse"/>
+	</fieldset>
+	<input type="submit" name="submit" value="Se connecter"/>
+</form>
