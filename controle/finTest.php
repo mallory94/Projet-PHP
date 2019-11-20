30affef6bd4b6aefc093cd8ec8b2ffb07835313d
mallory94 <malloga@hotmail.fr>
2019-11-20 12:10:37 +0100

    correction de la redirection lors de la desactivation d'un test (partie prof)

--- controle/finTest.php
+++ controle/finTest.php
@@ -3,6 +3,5 @@
     var_dump($_SESSION['idtest'][0]['id_test']);
 	require_once ('../modele/testBD.php');
 	arreterTestBD($_SESSION['idtest'][0]['id_test']);
-	//header ("Location: index.php?controle=utilisateur&action=accueil" );
-
+	header ("Location: ../index.php?controle=utilisateur&action=accueil" );
 ?>
